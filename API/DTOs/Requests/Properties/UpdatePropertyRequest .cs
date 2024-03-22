134ef007f670c49330185114cec96c61ccd4e9f2
Hoang Cuong <hoangcuong04012001@gmail.com>
2024-03-22 11:33:59 +0700

    fix field UpdateProperty

--- API/DTOs/Requests/Properties/UpdatePropertyRequest .cs
+++ API/DTOs/Requests/Properties/UpdatePropertyRequest .cs
@@ -5,25 +5,27 @@ namespace API.DTOs.Requests.Properties
 {
     public class UpdatePropertyRequest : IMapTo<Property>
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        public string Code { get; set; }
+        public string? Code { get; set; }
 
         public List<string>? Images { get; set; }
 
-        public string Street { get; set; }
+        public string? Street { get; set; }
 
-        public string Ward { get; set; }
+        public string? Ward { get; set; }
 
-        public string District { get; set; }
+        public string? District { get; set; }
 
-        public string City { get; set; }
+        public string? City { get; set; }
 
-        public double Area { get; set; }
+        public double? Area { get; set; }
 
-        public double RevervePrice { get; set; }
+        public double? RevervePrice { get; set; }
 
-        public bool isAvailable { get; set; }
+        public bool? isAvailable { get; set; }
+
+        public bool? isDone { get; set; }
     }
 }
     
